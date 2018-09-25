abd8fb19374a3aae261966615ce49563f02e01d1
AdelNorberg <darzemanov101@mail.com>
2018-09-25 13:59:50 +0300

    new refactoring
    Рефакторинг глобальных стилей, обновление логики

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,32 +6,16 @@
           <v-toolbar-title class="display-1">Norberg</v-toolbar-title>
         </nuxt-link>
         <v-spacer/>
-        <nuxt-link to="/news">
+        <nuxt-link
+          v-for="(header, i) in headers"
+          :key="i"
+          :to="header.path"
+        >
           <v-btn
             flat
-            dark>
-            NEWS
-          </v-btn>
-        </nuxt-link>
-        <nuxt-link to="/">
-          <v-btn
-            flat
-            dark>
-            HOME
-          </v-btn>
-        </nuxt-link>
-        <nuxt-link to="/about">
-          <v-btn
-            flat
-            dark>
-            ABOUT
-          </v-btn>
-        </nuxt-link>
-        <nuxt-link to="/problem">
-          <v-btn
-            flat
-            dark>
-            PROBLEM
+            dark
+          >
+            {{ header.name }}
           </v-btn>
         </nuxt-link>
         <v-spacer/>
@@ -39,7 +23,8 @@
           dark
           depressed
           large
-          color="#5c85c9">
+          color="#5c85c9"
+        >
           <v-icon dark>search</v-icon>
         </v-btn>
       </v-layout>
@@ -57,71 +42,40 @@ import footerContainer from '@/components/footer-container'
 export default {
   components: {
     footerContainer
+  },
+  data () {
+    return {
+      headers: [{
+          name: 'NEWS',
+          path: '/news'
+        },
+        {
+          name: 'HOME',
+          path: '/'
+        },
+        {
+          name: 'ABOUT',
+          path: '/about'
+        },
+        {
+          name: 'PROBLEM',
+          path: '/problem'
+        }
+      ]
+    }
   }
 }
 </script>
 
-<style>
-html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
+<style lang="sass" scoped>
+.gl-v-container
+  margin-top: -12rem
 
-*, *:before, *:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-a {
-  text-decoration: none;
-  color: white;
-}
-
-.gl-v-container {
-  margin-top: -12rem;
-}
-
-.gl-toolbar {
-  padding-top: 1.8rem;
-  padding-left: 1rem;
-  padding-right: 1rem;
-  height: 25rem;
-  background-color: #1f74ff;
+.gl-toolbar
+  padding-top: 1.8rem
+  padding-left: 1rem
+  padding-right: 1rem
+  height: 25rem
+  background-color: #1f74ff
   color: rgb(255, 255, 255)
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
 </style>
--- layouts/news.vue
+++ layouts/news.vue
@@ -1,50 +1,42 @@
 <template>
-  <v-layout
-    column
-    justify-space-between
-    align-content-space-between>
-    <div class="gl-toolbar">
-      <v-layout align-center>
-        <nuxt-link to="/landing">
-          <v-toolbar-title class="display-1">Norberg</v-toolbar-title>
-        </nuxt-link>
-        <v-spacer/>
-        <nuxt-link to="/">
+  <div class="container-layout">
+    <v-layout column>
+      <div class="gl-toolbar">
+        <v-layout align-center>
+          <nuxt-link to="/landing">
+            <v-toolbar-title class="display-1">Norberg</v-toolbar-title>
+          </nuxt-link>
+          <v-spacer/>
+          <nuxt-link
+            v-for="(header, i) in headers"
+            :key="i"
+            :to="header.path"
+          >
+            <v-btn
+              flat
+              dark
+            >
+              {{ header.name }}
+            </v-btn>
+          </nuxt-link>
+          <v-spacer/>
           <v-btn
-            flat
-            dark>HOME</v-btn>
-        </nuxt-link>
-        <nuxt-link to="/news">
-          <v-btn
-            flat
-            dark>NEWS</v-btn>
-        </nuxt-link>
-        <nuxt-link to="/noname">
-          <v-btn
-            flat
-            dark>NONAME</v-btn>
-        </nuxt-link>
-        <nuxt-link to="/contact">
-          <v-btn
-            flat
-            dark>CONTACT</v-btn>
-        </nuxt-link>
-        <v-spacer/>
-        <v-btn
-          dark
-          depressed
-          large
-          color="#5c85c9">
-          <v-icon dark>search</v-icon>
-        </v-btn>
-      </v-layout>
-    </div>
-    <course-bar/>
-    <v-container class="gl-v-container">
-      <nuxt/>
-    </v-container>
-    <footer-container/>
-  </v-layout>
+            dark
+            depressed
+            large
+            color="#5c85c9"
+          >
+            <v-icon dark>search</v-icon>
+          </v-btn>
+        </v-layout>
+      </div>
+      <course-bar/>
+      <v-container class="gl-v-container">
+        <nuxt/>
+      </v-container>
+      <footer-container/>
+    </v-layout>
+  </div>
 </template>
 
 <script>
@@ -55,75 +47,43 @@ export default {
   components: {
     courseBar,
     footerContainer
+  },
+  data () {
+    return {
+      headers: [{
+          name: 'HOME',
+          path: '/'
+        },
+        {
+          name: 'NEWS',
+          path: '/news'
+        },
+        {
+          name: 'NONAME',
+          path: '/noname'
+        },
+        {
+          name: 'CONTACT',
+          path: '/contact'
+        }
+      ]
+    }
   }
 }
 </script>
 
-<style lang="css">
-html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
+<style lang="sass" scoped>
 
-*, *:before, *:after {
-  box-sizing: border-box;
-  margin: 0;
-}
+.container-layout
+  min-height: 100%
 
-body {
-  min-height: 100%;
-}
+.gl-v-container
 
-a {
-  text-decoration: none;
-  color: white;
-}
-
-.gl-v-container {
-  margin-top: -12rem;
-}
-
-.gl-toolbar {
-  padding-top: 1.8rem;
-  padding-left: 1rem;
-  padding-right: 1rem;
-  height: 7rem;
-  background-color: #1f74ff;
+.gl-toolbar
+  padding-top: 1.8rem
+  padding-left: 1rem
+  padding-right: 1rem
+  height: 7rem
+  background-color: #1f74ff
   color: rgb(255, 255, 255)
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
 </style>
